e03c2e05e1edd1c6daa901a600065eb79840e1a0
Chris Blakley <chrisb@pinckneyhugo.com>
2014-05-06 09:36:25 -0400

    IE conditional classes (IE10) and fixed simantics
    Updated to include lt-ie10 classes and fixed the "lt-..." classes to not include themselves- instead, changed to "lte". So, IE8 will have "lt-ie9" and "lt-ie10", but now it also has "lte-ie8".

--- header.php
+++ header.php
@@ -3,10 +3,10 @@
  * Theme Header
  */
 ?><!DOCTYPE html>
-<!--[if lt IE 7 ]><html <?php language_attributes(); ?> class="no-js ie ie6 lt-ie7 lt-ie8 lt-ie9"><![endif]-->
-<!--[if IE 7 ]><html <?php language_attributes(); ?> class="no-js ie ie7 lt-ie7 lt-ie8 lt-ie9"><![endif]-->
-<!--[if IE 8 ]><html <?php language_attributes(); ?> class="no-js ie ie8 lt-ie8 lt-ie9"><![endif]-->
-<!--[if IE 9 ]><html <?php language_attributes(); ?> class="no-js ie ie9 lt-ie9"><![endif]-->
+<!--[if lt IE 7 ]><html <?php language_attributes(); ?> class="no-js ie ie6 lt-ie7 lt-ie8 lt-ie9 lt-ie10"><![endif]-->
+<!--[if IE 7 ]><html <?php language_attributes(); ?> class="no-js ie ie7 lte-ie7 lt-ie8 lt-ie9 lt-ie10"><![endif]-->
+<!--[if IE 8 ]><html <?php language_attributes(); ?> class="no-js ie ie8 lte-ie8 lt-ie9 lt-ie10"><![endif]-->
+<!--[if IE 9 ]><html <?php language_attributes(); ?> class="no-js ie ie9 lte-ie9 lt-ie10"><![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--><html <?php language_attributes(); ?> class="<?php echo (array_key_exists('debug', $_GET)) ? 'debug' : ''; ?> no-js"><!--<![endif]-->
 	<head>
 		<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
