d03cb791e878e36197cf467656404f1147a01b59
Chris Blakley <chrisb@pinckneyhugo.com>
2014-05-21 00:27:43 -0400

    Update tag.php

--- tag.php
+++ tag.php
@@ -9,7 +9,7 @@ get_header(); ?>
 	<div class="row">
 		<div class="ten columns">
 			<? the_breadcrumb(); ?>
-			<h1><?php printf( __( 'Tag Archives: %s', 'boilerplate' ), '' . single_tag_title( '', false ) . '' ); ?></h1>
+			<h1><?php printf( 'Tag Archives: %s', '' . single_tag_title( '', false ) . '' ); ?></h1>
 			<?php get_template_part( 'loop', 'tag' ); ?>
 		</div><!--/columns-->
 		<div class="five columns push_one">
