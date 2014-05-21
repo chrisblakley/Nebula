056505cdad95dc257c918c241a756dd3c446f269
Chris Blakley <chrisb@pinckneyhugo.com>
2014-05-21 00:15:54 -0400

    Update 404.php

--- 404.php
+++ 404.php
@@ -10,8 +10,8 @@ get_header(); ?>
 		<div class="ten columns">
 			<? the_breadcrumb(); ?>
 			<article id="post-0" class="post error404 not-found" role="main">
-				<h1><?php _e( 'Not Found', 'boilerplate' ); ?></h1>
-				<p><?php _e( 'The page you requested could not be found.', 'boilerplate' ); ?></p>
+				<h1>Not Found</h1>
+				<p>The page you requested could not be found.</p>
 				<?php get_search_form(); echo '<script>document.getElementById(\'s\') && document.getElementById(\'s\').focus();</script>'.PHP_EOL; ?>
 			</article>
 		</div><!--/columns-->
