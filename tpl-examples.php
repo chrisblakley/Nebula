a8c30bba2c11e66c641dc02d2fc487b000729cfc
Chris Blakley <chrisb@pinckneyhugo.com>
2014-05-22 08:15:59 -0400

    Created example template
    This template will be a page of example integrations of interesting features like Google Maps and Youtube.

---
+++ tpl-examples.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Template Name: Examples
+ */
+
+get_header(); ?>
+
+<div class="container">
+	<div class="row">
+		<div class="sixteen columns">
+			<? the_breadcrumb(); ?>
+			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+					<div class="entry-content">
+						<?php the_content(); ?>
+						
+						<?php wp_link_pages( array( 'before' => '' . 'Pages:', 'after' => '' ) ); ?>
+						<?php edit_post_link( 'Edit', '<p class="edit-link">', '</p>' ); ?>
+					</div><!-- .entry-content -->
+				</article><!-- #post-## -->
+				<?php //comments_template( '', true ); ?>
+			<?php endwhile; ?>
+		</div><!--/columns-->
+	</div><!--/row-->
+</div><!--/container-->
+
+<div class="container socialcon">
+	<div class="row">
+		<div class="eight columns fb-like-box-con">
+			<div id="fbcon">
+				<div class="fbhead">
+					<p><a href="https://www.facebook.com/PinckneyHugo" target="_blank"><i class="icon-facebook"></i>Facebook</a></p>
+				</div><!--/fbhead-->
+				<div class="fbbody">
+					<div class="container">
+						<div class="fb-feed">
+							<div class="row tweetcon">
+								<div class="four columns">
+									<div class="fbicon"><a href="https://www.facebook.com/PinckneyHugo" target="_blank"><img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash3/s160x160/64307_10150605580729671_377991150_a.jpg"/></a></div>
+								</div><!--/columns-->
+								<div class="twelve columns">
+									<div class="fbuser">
+										<a href="https://www.facebook.com/PinckneyHugo" target="_blank">Pinckney Hugo Group</a>
+									</div><!--/fbuser-->
+									<div class="fbpost">
+										<?php echo do_shortcode('[custom-facebook-feed id=PinckneyHugo num=1]'); //This is currently using one of my access tokens. We need to create one for WineGuardian using their FB login creds. ?>
+									</div><!--/fbpost-->
+								</div><!--/columns-->
+							</div><!--/row-->
+						</div><!--/fb-feed-->
+					</div><!--/container-->
+				</div><!--/fbbody-->
+			</div><!--/fbcon-->
+		</div><!--/columns-->
+		<div class="eight columns">
+			<div id="twittercon">
+				<div class="twitterhead">
+					<p><a href="https://twitter.com/pinckneyhugo" target="_blank"><i class="icon-twitter"></i>Tweets</a></p>
+				</div><!--/twitterhead-->
+				<div class="twitterbody">
+					<div class="container">
+						<div class="twitter-feed">		
+							<div id="twitter_update_list"></div>
+						</div><!--/twitter-feed-->
+					</div><!--/container-->
+				</div><!--/twitterbody-->
+			</div><!--/twittercon-->
+		</div><!--/columns-->
+	</div><!--/row-->
+</div><!--/container-->
+
+<?php get_footer(); ?>
