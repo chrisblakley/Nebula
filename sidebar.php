0358a9ee4a623c406e494560dae5d46e539d0995
Chris Blakley <chrisb@pinckneyhugo.com>
2014-05-22 09:11:37 -0400

    Updated cForm7 Example
    Added CAPTCHA example

--- sidebar.php
+++ sidebar.php
@@ -39,47 +39,55 @@
 	
 	<li>
 		
-<?php if (1==2) :  //Sample Contact Form 7 form - FORM SECTION in WP Admin (If using this snippet for non-sidebar cForm, wrap it in a <ul>) ?><!--
+<?php if (1==2) {  //Sample Contact Form 7 form - FORM SECTION in WP Admin (If using this snippet for non-sidebar cForm, wrap it in a <ul>) 
+	/*
+	<li class="field">
+		[text* name class:text class:input class:cform7-name placeholder "Your Name*"]
+	</li>
+	<li class="field">
+		[email* email class:email class:input class:cform7-email placeholder "Email Address*"]
+	</li>
+	<li class="field">
+		[text phone class:numeric class:input class:cform7-phone placeholder "Phone Number"]
+	</li>
+	</li>
+	<li class="field">
+		[text bday class:numeric class:input class:cform7-bday placeholder "Date of Birth"]
+	</li>
+	<li class="field">
+		[textarea* message class:textarea class:input class:cform7-message placeholder "Enter your message here.*"]
+	</li>
+	<li class="field">
+		[file file-154 limit:6000000 filetypes:pdf|doc|docx|jpg|jpeg|gif|png|tiff]
+	</li>
+	<!-- CAPTCHA requires the plugin Really Simple CAPTCHA
+		<li class="field">
+			[captchac captcha-946 id:captcha-image size:m]
+			[captchar captcha-946 id:captcha-input class:input placeholder "Enter above CAPTCHA text here"]
+		</li>
+	-->
+	<li class="field">
+		[submit class:medium class:primary class:btn class:submit submit"Send"]
+	</li>
+	
+	//Sample Contact Form 7 form - MESSAGE BODY in WP Admin. Add [file-154] to "File attachments".
+	From: [name] <[email]>
+	[phone]
+	[bday]
+	
+	Message Body:
+	[message]
+	
+	
+	This e-mail was sent from a contact form on Website Name (http://domain.com)
+	
+	
+	//Sample Contact Form 7 form - ADDITIONAL SETTINGS in WP Admin. Change "sidebar" to the name of the form being used.
+	on_sent_ok: "cFormSuccess('sidebar');"
+	*/
+}
 
-<li class="field">
-	[text* name class:text class:input class:cform7-name placeholder "Your Name*"]
-</li>
-<li class="field">
-	[email* email class:email class:input class:cform7-email placeholder "Email Address*"]
-</li>
-<li class="field">
-	[text phone class:numeric class:input class:cform7-phone placeholder "Phone Number"]
-</li>
-</li>
-<li class="field">
-	[text bday class:numeric class:input class:cform7-bday placeholder "Date of Birth"]
-</li>
-<li class="field">
-	[textarea* message class:textarea class:input class:cform7-message placeholder "Enter your message here.*"]
-</li>
-<li class="field">
-	[file file-154 limit:6000000 filetypes:pdf|doc|docx|jpg|jpeg|gif|png|tiff]
-</li>
-<li class="field">
-	[submit class:medium class:primary class:btn class:submit submit"Send"]
-</li>
-
-//Sample Contact Form 7 form - MESSAGE BODY in WP Admin. Add [file-154] to "File attachments".
-From: [name] <[email]>
-[phone]
-[bday]
-
-Message Body:
-[message]
-
-
-This e-mail was sent from a contact form on PHG Sandbox (http://phgtestsite.com/wordpress_sandbox)
-
-
-//Sample Contact Form 7 form - ADDITIONAL SETTINGS in WP Admin. Change "sidebar" to the name of the form being used.
-on_sent_ok: "cFormSuccess('sidebar');"
-
---><?php endif; ?>
+?>
 
 <h3>Contact Us</h3>
 <ul id="cform7-container">
