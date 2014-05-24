b0dacce607b4f38e0713b6792d5f84fd25a77437
Chris Blakley <chrisb@pinckneyhugo.com>
2014-05-24 01:29:13 -0400

    Update tpl-homepage.php

--- tpl-homepage.php
+++ tpl-homepage.php
@@ -12,23 +12,17 @@ get_header(); ?>
 <div class="container">
 	<div class="row">
 		<div class="ten columns">
-			<p>This is a sample listing page using query_posts() and the while loop and pagination using wp_pagenavi.</p>
-			<ul class="postscon">
-				<?php query_posts( 'post_type=movie', array( 'showposts' => 3, 'paged' => get_query_var('paged') ) ); //@TODO: Remove post_type to query all posts. ?>
-					<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-						<li>
-							<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-								<h2 class="entry-title"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
-								<div class="entry-content">
-									<?php echo nebula_the_excerpt('Read more &raquo;', 20, 1); ?>
-									<p><?php edit_post_link( 'Edit', '', '' ); ?></p>
-								</div><!-- .entry-content -->
-							</article><!-- #post-## -->
-						</li>
-					<?php endwhile; ?>
-					<?php wp_pagenavi(); ?>
-				<?php wp_reset_query(); ?>
-			</ul><!--/postscon-->
+			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+					<div class="entry-content">
+						<?php the_content(); ?>
+						
+						<?php wp_link_pages( array( 'before' => '' . 'Pages:', 'after' => '' ) ); ?>
+						<?php edit_post_link('Edit'); ?>
+					</div><!-- .entry-content -->
+				</article><!-- #post-## -->
+			<?php endwhile; ?>
 		</div><!--/columns-->
 		<div class="five columns push_one">
 			<?php get_sidebar(); ?>
