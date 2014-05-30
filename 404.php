dae3d08519a2d644ec5cc998aff1185f576d3c59
Chris Blakley <chrisb@pinckneyhugo.com>
2014-05-30 17:19:52 -0400

    Global defer/async

--- 404.php
+++ 404.php
@@ -7,6 +7,7 @@ get_header(); ?>
 
 <div class="container">
 	<div class="row">
+		
 		<div class="ten columns">
 			<? the_breadcrumb(); ?>
 			<article id="post-0" class="post error404 not-found" role="main">
@@ -15,13 +16,16 @@ get_header(); ?>
 				<?php get_search_form(); echo '<script>document.getElementById(\'s\') && document.getElementById(\'s\').focus();</script>'.PHP_EOL; ?>
 			</article>
 		</div><!--/columns-->
+		
 		<div class="five columns push_one">
 			<?php get_sidebar(); ?>
 		</div><!--/columns-->
+		
 	</div><!--/row-->
 </div><!--/container-->
 
-<script defer>
+<?php global $defer; global $async; ?>
+<script <?php echo $defer; ?>>
 	if ( document.referrer.length ) {
 		ga('send', 'event', '404 Not Found', 'Request: ' + document.URL, 'Referrer: ' + document.referrer);
 	} else {
