e8e47f135521d5719f0d8df88e61856be293ee84
Chris Blakley <chrisb@pinckneyhugo.com>
2014-05-30 17:28:18 -0400

    Initial simplification

--- author.php
+++ author.php
@@ -1,50 +1,25 @@
 <?php
 /**
  * The template for displaying Author Archive pages.
- *
- * @package WordPress
- * @subpackage Boilerplate
- * @since Boilerplate 1.0
  */
 
 get_header(); ?>
 
+<?php if ( have_posts() ) { the_post(); } //Queue the first post then reset it before the loop. ?>
+	<h1>Author Archives: <a href="<?php get_author_posts_url( get_the_author_meta('ID') ); ?>"><?php echo get_the_author(); ?></a></h1>
 <?php
-	/* Queue the first post, that way we know who
-	 * the author is when we try to get their name,
-	 * URL, description, avatar, etc.
-	 *
-	 * We reset this later so we can run the loop
-	 * properly with a call to rewind_posts().
-	 */
-	if ( have_posts() )
-		the_post();
-?>
 
-				<h1><?php printf( 'Author Archives: %s', "<a class='url fn n' href='" . get_author_posts_url( get_the_author_meta( 'ID' ) ) . "' title='" . esc_attr( get_the_author() ) . "' rel='me'>" . get_the_author() . "</a>" ); ?></h1>
+if ( get_the_author_meta('description') ) : ?>
 
-<?php
-// If a user has filled out their description, show a bio on their entries.
-if ( get_the_author_meta( 'description' ) ) : ?>
-
-							<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'boilerplate_author_bio_avatar_size', 60 ) ); ?>
-							<h2><?php printf( 'About %s', get_the_author() ); ?></h2>
-							<?php the_author_meta( 'description' ); ?>
+	<?php echo get_avatar( get_the_author_meta('user_email'), apply_filters('boilerplate_author_bio_avatar_size', 60) ); //Update author bio avatar image name ?>
+	<h2>About <?php echo get_the_author(); //@TODO: Need to echo? ?></h2>
+	<?php the_author_meta('description'); ?>
 
 <?php endif; ?>
 
 <?php
-	/* Since we called the_post() above, we need to
-	 * rewind the loop back to the beginning that way
-	 * we can run the loop properly, in full.
-	 */
 	rewind_posts();
-
-	/* Run the loop for the author archive page to output the authors posts
-	 * If you want to overload this in a child theme then include a file
-	 * called loop-author.php and that will be used instead.
-	 */
-	 get_template_part( 'loop', 'author' );
+	get_template_part('loop', 'author');
 ?>
 
 <?php get_sidebar(); ?>
