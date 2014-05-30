ad41a31eb1aef2b5ae10b4e76d697761eb029cdb
Chris Blakley <chrisb@pinckneyhugo.com>
2014-05-30 17:20:12 -0400

    Global defer/async

--- tag.php
+++ tag.php
@@ -7,14 +7,17 @@ get_header(); ?>
 
 <div class="container">
 	<div class="row">
+		
 		<div class="ten columns">
 			<? the_breadcrumb(); ?>
-			<h1><?php printf( 'Tag Archives: %s', '' . single_tag_title( '', false ) . '' ); ?></h1>
-			<?php get_template_part( 'loop', 'tag' ); ?>
+			<h1>Tag Archives: <?php single_tag_title('', false); ?></h1>
+			<?php get_template_part('loop', 'tag'); ?>
 		</div><!--/columns-->
+		
 		<div class="five columns push_one">
 			<?php get_sidebar(); ?>
 		</div><!--/columns-->
+		
 	</div><!--/row-->
 </div><!--/container-->
 
