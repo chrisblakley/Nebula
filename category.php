b04741a68e601bd3d610106167d025a5d72efb63
Chris Blakley <chrisb@pinckneyhugo.com>
2014-05-30 17:18:37 -0400

    Simplification

--- category.php
+++ category.php
@@ -7,19 +7,23 @@ get_header(); ?>
 
 <div class="container">
 	<div class="row">
+		
 		<div class="ten columns">
 			<? the_breadcrumb(); ?>
-			<h1><?php printf( 'Category Archives: %s', '' . single_cat_title( '', false ) . '' ); ?></h1>
+			<h1>Category Archives: <?php single_cat_title('', false); ?></h1>
 				<?php
 					$category_description = category_description();
-					if ( ! empty( $category_description ) )
+					if ( !empty($category_description) ) {
 						echo '' . $category_description . '';
-						get_template_part( 'loop', 'category' );
-					?>
+						get_template_part('loop', 'category');
+					}
+				?>
 		</div><!--/columns-->
+		
 		<div class="five columns push_one">
 			<?php get_sidebar(); ?>
 		</div><!--/columns-->
+		
 	</div><!--/row-->
 </div><!--/container-->
 
