c34a8efe0e1cc23f0145e81a96a4dc26880a5080
Chris Blakley <chrisb@pinckneyhugo.com>
2014-05-31 00:26:09 -0400

    Entry Manage

--- page.php
+++ page.php
@@ -7,6 +7,7 @@ get_header(); ?>
 
 <div class="container">
 	<div class="row">
+		
 		<div class="ten columns">
 			<? the_breadcrumb(); ?>
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
@@ -14,17 +15,29 @@ get_header(); ?>
 					<h1 class="entry-title"><?php the_title(); ?></h1>
 					<div class="entry-content">
 						<?php the_content(); ?>
-						
-						<?php wp_link_pages( array( 'before' => '' . 'Pages:', 'after' => '' ) ); ?>
-						<?php edit_post_link('Edit'); ?>
+												
+						<?php if ( current_user_can('manage_options') ) : ?>
+							<div class="container entry-manage">
+								<div class="row">
+									<div class="six columns entry-edit">
+										<a href="<?php echo get_admin_url(); ?>" target="_blank">Admin</a> | <?php edit_post_link('Edit'); ?>
+									</div><!--/columns-->
+									<div class="ten columns">
+										<span class="entry-modified">Last Modified: <strong><?php the_modified_date(); ?></strong> by <strong><?php the_modified_author(); ?></strong></span>
+									</div><!--/columns-->
+								</div>
+							</div>
+						<?php endif; ?>
 					</div><!-- .entry-content -->
 				</article><!-- #post-## -->
 				<?php //comments_template( '', true ); ?>
 			<?php endwhile; ?>
 		</div><!--/columns-->
+		
 		<div class="five columns push_one">
 			<?php get_sidebar(); ?>
 		</div><!--/columns-->
+		
 	</div><!--/row-->
 </div><!--/container-->
 
