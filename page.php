734fa80743dccf453d6dd49339d02ee957c1187d
Chris Blakley <chrisb@pinckneyhugo.com>
2014-05-31 12:47:40 -0400

    Added nebula_manage()

--- page.php
+++ page.php
@@ -7,9 +7,13 @@ get_header(); ?>
 
 <div class="container">
 	<div class="row">
+		<div class="sixteen columns">
+			<? the_breadcrumb(); ?>
+			<hr/>
+		</div><!--/columns-->
+	<div class="row">
 		
 		<div class="ten columns">
-			<? the_breadcrumb(); ?>
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<h1 class="entry-title"><?php the_title(); ?></h1>
@@ -19,11 +23,8 @@ get_header(); ?>
 						<?php if ( current_user_can('manage_options') ) : ?>
 							<div class="container entry-manage">
 								<div class="row">
-									<div class="six columns entry-edit">
-										<a href="<?php echo get_admin_url(); ?>" target="_blank">Admin</a> | <?php edit_post_link('Edit'); ?>
-									</div><!--/columns-->
-									<div class="ten columns">
-										<span class="entry-modified">Last Modified: <strong><?php the_modified_date(); ?></strong> by <strong><?php the_modified_author(); ?></strong></span>
+									<div class="sixteen columns">
+										<?php nebula_manage('edit'); ?> <?php nebula_manage('modified'); ?>
 									</div><!--/columns-->
 								</div>
 							</div>
