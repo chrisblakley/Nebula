bc4b17687a62467573a8810ed27a11e532bf42ee
Chris Blakley <chrisb@pinckneyhugo.com>
2014-05-31 00:24:37 -0400

    Added Gumby framework

--- author.php
+++ author.php
@@ -5,22 +5,34 @@
 
 get_header(); ?>
 
-<?php if ( have_posts() ) { the_post(); } //Queue the first post then reset it before the loop. ?>
-	<h1>Author Archives: <a href="<?php get_author_posts_url( get_the_author_meta('ID') ); ?>"><?php echo get_the_author(); ?></a></h1>
-<?php
+<div class="container">
+	<div class="row">
+		<div class="ten columns">
 
-if ( get_the_author_meta('description') ) : ?>
+			<?php if ( have_posts() ) { the_post(); } //Queue the first post then reset it before the loop. ?>
+				<h1>Author Archives: <a href="<?php get_author_posts_url( get_the_author_meta('ID') ); ?>"><?php echo get_the_author(); ?></a></h1>
+			<?php
+			
+			if ( get_the_author_meta('description') ) : ?>
+			
+				<?php echo get_avatar( get_the_author_meta('user_email'), apply_filters('boilerplate_author_bio_avatar_size', 60) ); //Update author bio avatar image name ?>
+				<h2>About <?php echo get_the_author(); ?></h2>
+				<?php the_author_meta('description'); ?>
+			
+			<?php endif; ?>
+			
+			<?php
+				rewind_posts();
+				get_template_part('loop', 'author');
+			?>
 
-	<?php echo get_avatar( get_the_author_meta('user_email'), apply_filters('boilerplate_author_bio_avatar_size', 60) ); //Update author bio avatar image name ?>
-	<h2>About <?php echo get_the_author(); //@TODO: Need to echo? ?></h2>
-	<?php the_author_meta('description'); ?>
+		</div><!--/columns-->
+		
+		<div class="five columns push_one">
+			<?php get_sidebar(); ?>
+		</div><!--/columns-->
+		
+	</div><!--/row-->
+</div><!--/container-->
 
-<?php endif; ?>
-
-<?php
-	rewind_posts();
-	get_template_part('loop', 'author');
-?>
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
