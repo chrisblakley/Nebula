a7587863ac4cffd6dfe512e18bd2d3ea3dfa0be8
Chris Blakley <chrisb@pinckneyhugo.com>
2014-05-31 00:25:47 -0400

    Entry Manage

--- single.php
+++ single.php
@@ -7,24 +7,42 @@ get_header(); ?>
 
 <div class="container">
 	<div class="row">
+		
 		<div class="ten columns">
 			<? the_breadcrumb(); ?>
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<h1 class="entry-title"><?php the_title(); ?></h1>
-					<?php nebula_meta('on'); ?> <?php nebula_meta('in'); ?>
+					
+					<div class="entry-meta">
+			        	<?php nebula_meta('on', 0); ?> <?php nebula_meta('cat'); ?> <?php nebula_meta('by'); ?> <?php nebula_meta('tags'); ?>
+			        </div>
+					
 					<div class="entry-content">
 						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '' . 'Pages:', 'after' => '' ) ); ?>
-						<?php edit_post_link( 'Edit', '<p class="edit-link">', '</p>' ); ?>
+						
+						<?php if ( current_user_can('manage_options') ) : ?>
+							<div class="container entry-manage">
+								<div class="row">
+									<div class="six columns entry-edit">
+										<a href="<?php echo get_admin_url(); ?>" target="_blank">Admin</a> | <?php edit_post_link('Edit'); ?>
+									</div><!--/columns-->
+									<div class="ten columns">
+										<span class="entry-modified">Last Modified: <strong><?php the_modified_date(); ?></strong> by <strong><?php the_modified_author(); ?></strong></span>
+									</div><!--/columns-->
+								</div>
+							</div>
+						<?php endif; ?>
 					</div><!-- .entry-content -->
 				</article><!-- #post-## -->
 				<?php //comments_template( '', true ); ?>
 			<?php endwhile; ?>
 		</div><!--/columns-->
+		
 		<div class="five columns push_one">
 			<?php get_sidebar(); ?>
 		</div><!--/columns-->
+		
 	</div><!--/row-->
 </div><!--/container-->
 
