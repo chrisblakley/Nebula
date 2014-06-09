28edb6f4b3cc7bddc9135190ad486fa00f101859
Chris Blakley <chrisb@pinckneyhugo.com>
2014-06-08 23:26:02 -0400

    CSS Browser Selector example

--- tpl-examples.php
+++ tpl-examples.php
@@ -30,7 +30,36 @@ pre.js, pre.javascript {border: 1px solid #f1bf26; border-left: 5px solid #f1bf2
 .pretitle.php {color: #5a68a5;}
 pre.php {border: 1px solid #5a68a5; border-left: 5px solid #5a68a5;}
 	pre.php::selection {background: #c8cde1;}
+	
+/* CSS Browser Selector Example Styles */
+div.cssbs {position: relative; display: table; height: 150px; border: 1px solid #222;}
+	div.cssbs:after {width: 100%; height: 100%; line-height: 17px; color: #fff; text-align: center; font-family: 'FontAwesome'; white-space: pre; display: table-cell; vertical-align: middle;}
+	div.cssbs:before {content: ''; position: absolute; width: 100%; height: 100%; background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.2));}
+	.win.chrome div.cssbs {background: #4884b8;} .win.chrome div.cssbs:after {content: '\f17a \00A0 Chrome on Windows \A 2008-';}
+	.mac.chrome div.cssbs {background: #4884b8;} .mac.chrome div.cssbs:after {content: '\f179 \00A0 Chrome on Mac \A 2008-';}
+	.linux.chrome div.cssbs {background: #4884b8;} .linux.chrome div.cssbs:after {content: '\f17c \00A0 Chrome on Linux \A 2008-';}
+	.win.firefox div.cssbs {background: #dc5d27; border: 2px solid #b31b27;} .win.firefox div.cssbs:after {content: '\f17a \00A0 Firefox on Windows \A 2002-';}
+	.mac.firefox div.cssbs {background: #dc5d27; border: 2px solid #b31b27;} .mac.firefox div.cssbs:after {content: '\f179 \00A0 Firefox on Mac \A 2002-';}
+	.linux.firefox div.cssbs {background: #dc5d27; border: 2px solid #b31b27;} .linux.firefox div.cssbs:after {content: '\f17c \00A0 Firefox on Linux \A 2002-';}
+	.safari div.cssbs {background: #42aeda; border: 2px solid #a1a1a1;} .safari div.cssbs:after {content: '\f179 \00A0 Safari \A 2003-';}
+	.opera div.cssbs {background: #e53141; border: 2px solid #9b1624;} .opera div.cssbs:after {content: 'Opera \A 1995-';}
+	.ie div.cssbs {background: #2ebaee;} .ie div.cssbs:after {content: '\f17a \00A0 Internet Explorer \A 1995-';}
+	.ie5 div.cssbs {background: #3ea3e2;} .ie5 div.cssbs:after {content: '\f17a \00A0 Internet Explorer 5 \A 1998-';}
+	.ie6 div.cssbs {background: #3696e9; border: 2px solid #72f0fc;} .ie6 div.cssbs:after {content: '\f17a \00A0 Internet Explorer 6 \A 2001-2014';}
+	.ie7 div.cssbs {background: #1374ae; border: 2px solid #f4b619;} .ie7 div.cssbs:after {content: '\f17a \00A0 Internet Explorer 7 \A 2006-2017';}
+	.ie8 div.cssbs {background: #1374ae; border: 2px solid #f4b619;} .ie8 div.cssbs:after {content: '\f17a \00A0 Internet Explorer 8 \A 2009-2020';}
+	.ie9 div.cssbs {background: #3aa8de; border: 2px solid #fbd21e;} .ie9 div.cssbs:after {content: '\f17a \00A0 Internet Explorer 9 \A 2011-2020';}
+	.ie10 div.cssbs {background: #2b6bec;} .ie10 div.cssbs:after {content: '\f17a \00A0 Internet Explorer 10 \A 2012-2023';}
+	.ie11 div.cssbs {background: #2ebaee;} .ie11 div.cssbs:after {content: '\f17a \00A0 Internet Explorer 11 \A 2013-2023';}
+	.android div.cssbs {background: #a5c93a; border: 2px solid #a5c93a;} .android div.cssbs:after {content: '\f17b \00A0 Android \A 2008-';}
+	.android.chrome div.cssbs {background: #4884b8;} .android.chrome div.cssbs:after {content: '\f17b \00A0 Chrome on Android \A 2012-';}
+	.iphone div.cssbs {background: #42aeda; border: 2px solid #a1a1a1;} .iphone div.cssbs:after {content: '\f179 \00A0 iPhone \A 2007-';}
+	.iphone.chrome div.cssbs {background: #4884b8;} .iphone.chrome div.cssbs:after {content: '\f179 \00A0 Chrome on iPhone \A 2012-';}
+	.ipad div.cssbs {background: #42aeda; border: 2px solid #a1a1a1;} .ipad div.cssbs:after {content: '\f179 \00A0 iPad \A 2010-';}
+	.ipad.chrome div.cssbs {background: #4884b8;} .ipad.chrome div.cssbs:after {content: '\f179 \00A0 Chrome on iPad \A 2012-';}
 </style>
+
+<script src="<?php bloginfo('template_directory');?>/js/libs/cssbs.js" <?php echo $GLOBALS["async"]; ?>></script> <!-- This script is found in the footer. It is disabled by default, so it needs to be called again here to show the CSS Browser Selector example section. -->
 
 <script>
 	jQuery(document).ready(function() {	
@@ -42,6 +71,40 @@ pre.php {border: 1px solid #5a68a5; border-left: 5px solid #5a68a5;}
 		jQuery('pre.javascript').before('<span class="pretitle javascript">Javascript</span>');
 		jQuery('pre.php').before('<span class="pretitle php">PHP</span>');
 	});
+</script>
+
+<script>
+		var finalResult = [];
+			jQuery.ajax({
+			  url:'https://api.chartbeat.com/cbtotal/?v=3'
+			, type:'POST'
+			, dataType:'jsonp'
+			, async:true
+			, success:function(result){
+				var browser = result['bt'];
+				var live_count = result['n'];	
+				console.log(result);
+
+				jQuery.each(browser, function(i, k){
+					switch(i){
+						case 'chrome': var bt = 'Chrome'; break;
+						case 'ie': var bt = 'Internet Explorer'; break;
+						case 'ff': var bt = 'Firefox'; break;
+						case 'safari': var bt = 'Safari'; break;
+						default: var bt = 'Chrome'; break;
+					}
+					var percentage = Math.round(k / live_count * 100);
+					//console.log(percentage);
+					var obj = {};
+					obj.content = bt;
+					obj.description = "<match>"+bt+"</match><dim> : </dim><url>"+percentage+"%</url>"; 
+
+					finalResult.push(obj);
+
+				});
+					suggest(finalResult);
+				}
+			});
 </script>
 
 <section><!-- Do not duplicate this section because it has inline styles. -->
@@ -347,6 +410,18 @@ pre.php {border: 1px solid #5a68a5; border-left: 5px solid #5a68a5;}
 			<h2>CSS Browser Selector</h2>
 			<h5><a href="#" target="_blank">Documentation &raquo;</a></h5>
 			<p>Useful for debugging, but should only be enabled if feature detection (using Modernizr) will not work!</p>
+			
+			<div class="container">
+				<div class="row">
+					<div class="four columns">
+						<div class="cssbs"></div>
+					</div><!--/columns-->
+					<div class="twelve columns">
+						Here is text
+					</div><!--/columns-->
+				</div><!--/row-->
+			</div><!--/container-->
+			
 			<br/><br/><hr/>
 		</div><!--/columns-->
 	</div><!--/row-->
