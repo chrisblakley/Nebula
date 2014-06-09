bac6b66e772c25a5be10af218fdba9c254f14081
Chris Blakley <chrisb@pinckneyhugo.com>
2014-06-08 23:26:39 -0400

    Fixed space issue in dynamic html classes

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <!--[if IE 7 ]><html <?php language_attributes(); ?> class="no-js ie ie7 lte-ie7 lt-ie8 lte-ie8 lt-ie9 lte-ie9 lt-ie10"><![endif]-->
 <!--[if IE 8 ]><html <?php language_attributes(); ?> class="no-js ie ie8 lte-ie8 lt-ie9 lte-ie9 lt-ie10"><![endif]-->
 <!--[if IE 9 ]><html <?php language_attributes(); ?> class="no-js ie ie9 lte-ie9 lt-ie10"><![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--><html <?php language_attributes(); ?> class="<?php echo (array_key_exists('debug', $_GET)) ? 'debug' : ''; ?> <?php mobile_classes(); ?> no-js"><!--<![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><html <?php language_attributes(); ?> class=" <?php echo (array_key_exists('debug', $_GET)) ? 'debug' : ''; ?> <?php mobile_classes(); ?> no-js "><!--<![endif]-->
 	<head>
 		<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1' />
 		<meta charset="<?php bloginfo('charset'); ?>" />
@@ -41,7 +41,7 @@
 		<meta property="og:image" content="<?php bloginfo('template_directory');?>/images/og-thumb1.jpg" />
     	<meta property="og:image" content="<?php bloginfo('template_directory');?>/images/og-thumb2.jpg" />
 		<meta property="og:email" content="<?php echo get_option('admin_email', $admin_user->user_email); ?>" />
-		<meta property="og:phone_number" content="" /> <!-- Ex: "+1-315-478-6700" -->
+		<meta property="og:phone_number" content="" /> <!-- Ex: "+1-315-478-6700" --> <!-- Important: Enter this value as it will be default across the site! @TODO: Make a php variable with this val. -->
 		<meta property="og:fax_number" content="" /> <!-- Ex: "+1-315-478-6700" -->
 		<meta property="og:latitude" content="" />
 		<meta property="og:longitude" content="" />
