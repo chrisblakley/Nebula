9fded61104374fbf74c8a40898ab51d66524b621
Chris Blakley <chrisb@pinckneyhugo.com>
2014-06-17 00:34:08 -0400

    Updated framework
    Still needs some more updating, but doesn't appear broken anymore.

--- attachment.php
+++ attachment.php
@@ -1,14 +1,15 @@
 <?php
 /**
  * The template for displaying attachments.
- *
- * @package WordPress
- * @subpackage Boilerplate
- * @since Boilerplate 1.0
  */
 
 get_header(); ?>
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+<div class="container">
+	<div class="row">
+		
+		<div class="sixteen columns">
+			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 				<?php if ( ! empty( $post->post_parent ) ) : ?>
 					<p class="page-title"><a href="<?php echo get_permalink( $post->post_parent ); ?>" title="<?php esc_attr( printf( 'Return to %s', get_the_title( $post->post_parent ) ) ); ?>" rel="gallery"><?php
 						/* translators: %s - title of parent post */
@@ -16,85 +17,77 @@ get_header(); ?>
 					?></a></p>
 				<?php endif; ?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<h2 class="entry-title"><?php the_title(); ?></h2>
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+					
 					<div class="entry-meta">
-						<?php
-							printf('By %2$s',
-								'meta-prep meta-prep-author',
-								sprintf( '<a class="url fn n" href="%1$s" title="%2$s">%3$s</a>',
-									get_author_posts_url( get_the_author_meta( 'ID' ) ),
-									sprintf( esc_attr__( 'View all posts by %s', 'boilerplate' ), get_the_author() ),
-									get_the_author()
-								)
-							);
-						?>
+						By <a class="url fn n" href="<?php echo get_author_posts_url( get_the_author_meta('ID') ); ?>"><?php echo get_the_author(); ?></a>
 						<span>|</span>
-						<?php
-							printf( 'Published %2$s',
-								'meta-prep meta-prep-entry-date',
-								sprintf( '<abbr title="%1$s">%2$s</abbr>',
-									esc_attr( get_the_time() ),
-									get_the_date()
-								)
-							);
-							if ( wp_attachment_is_image() ) {
-								echo ' | ';
-								$metadata = wp_get_attachment_metadata();
-								printf( 'Full size is %s pixels',
-									sprintf( '<a href="%1$s" title="%2$s">%3$s &times; %4$s</a>',
-										wp_get_attachment_url(),
-										esc_attr( __('Link to full-size image', 'boilerplate') ),
-										$metadata['width'],
-										$metadata['height']
-									)
-								);
-							}
-						?>
-						<?php edit_post_link( 'Edit', '', '' ); ?>
+						Published <abbr><?php echo get_the_date(); ?></abbr>
+								
+						<?php if ( wp_attachment_is_image() ) : ?>
+							<?php $metadata = wp_get_attachment_metadata(); ?>
+							| Full size is <a href="<?php echo wp_get_attachment_url();  ?>" ><?php echo $metadata['width']; ?> &times; <?php echo $metadata['height']; ?></a>
+						<?php endif; ?>
 					</div><!-- .entry-meta -->
+			
 					<div class="entry-content">
 						<div class="entry-attachment">
-<?php if ( wp_attachment_is_image() ) :
-	$attachments = array_values( get_children( array( 'post_parent' => $post->post_parent, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID' ) ) );
-	foreach ( $attachments as $k => $attachment ) {
-		if ( $attachment->ID == $post->ID )
-			break;
-	}
-	$k++;
-	// If there is more than 1 image attachment in a gallery
-	if ( count( $attachments ) > 1 ) {
-		if ( isset( $attachments[ $k ] ) )
-			// get the URL of the next image attachment
-			$next_attachment_url = get_attachment_link( $attachments[ $k ]->ID );
-		else
-			// or get the URL of the first image attachment
-			$next_attachment_url = get_attachment_link( $attachments[ 0 ]->ID );
-	} else {
-		// or, if there's only 1 image attachment, get the URL of the image
-		$next_attachment_url = wp_get_attachment_url();
-	}
-?>
-							<p><a href="<?php echo $next_attachment_url; ?>" title="<?php echo esc_attr( get_the_title() ); ?>" rel="attachment"><?php
-								$attachment_size = apply_filters( 'boilerplate_attachment_size', 900 );
-								echo wp_get_attachment_image( $post->ID, array( $attachment_size, 9999 ) ); // filterable image width with, essentially, no limit for image height.
-							?></a></p>
-							<nav id="nav-below" class="navigation">
-								<div class="nav-previous"><?php previous_image_link( false ); ?></div>
-								<div class="nav-next"><?php next_image_link( false ); ?></div>
-							</nav><!-- #nav-below -->
-<?php else : ?>
-							<a href="<?php echo wp_get_attachment_url(); ?>" title="<?php echo esc_attr( get_the_title() ); ?>" rel="attachment"><?php echo basename( get_permalink() ); ?></a>
-<?php endif; ?>
+							<?php if ( wp_attachment_is_image() ) :
+								$attachments = array_values( get_children( array( 'post_parent' => $post->post_parent, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID' ) ) );
+								foreach ( $attachments as $k => $attachment ) {
+									if ( $attachment->ID == $post->ID )
+										break;
+									}
+									$k++;
+									// If there is more than 1 image attachment in a gallery
+									if ( count( $attachments ) > 1 ) {
+										if ( isset( $attachments[ $k ] ) )
+											// get the URL of the next image attachment
+											$next_attachment_url = get_attachment_link( $attachments[ $k ]->ID );
+										else
+											// or get the URL of the first image attachment
+											$next_attachment_url = get_attachment_link( $attachments[ 0 ]->ID );
+									} else {
+										// or, if there's only 1 image attachment, get the URL of the image
+										$next_attachment_url = wp_get_attachment_url();
+									}
+								?>
+								<p><a href="<?php echo $next_attachment_url; ?>" title="<?php echo get_the_title(); ?>">
+									<?php
+										$attachment_size = apply_filters( 'boilerplate_attachment_size', 900 );
+										echo wp_get_attachment_image( $post->ID, array( $attachment_size, 9999 ) ); // filterable image width with, essentially, no limit for image height.
+									?>
+								</a></p>
+				
+								<nav id="nav-below" class="navigation">
+									<div class="nav-previous"><?php previous_image_link( false ); ?></div>
+									<div class="nav-next"><?php next_image_link( false ); ?></div>
+								</nav><!-- #nav-below -->
+								
+							<?php else : ?>
+							
+								<a href="<?php echo wp_get_attachment_url(); ?>" title="<?php echo get_the_title(); ?>" ><?php echo basename( get_permalink() ); ?></a>
+								
+							<?php endif; ?>
 						</div><!-- .entry-attachment -->
 						<div class="entry-caption"><?php if ( !empty( $post->post_excerpt ) ) the_excerpt(); ?></div>
-<?php the_content( 'Continue reading &rarr;' ); ?>
-<?php wp_link_pages( array( 'before' => '' . 'Pages:', 'after' => '' ) ); ?>
+			
+						<?php the_content( 'Continue reading &rarr;' ); ?>
+						<?php wp_link_pages( array( 'before' => '' . 'Pages:', 'after' => '' ) ); ?>
+				
 						<footer class="entry-utility">
 							<?php boilerplate_posted_in(); ?>
 							<?php edit_post_link( 'Edit', ' <span class="edit-link">', '</span>' ); ?>
 						</footer><!-- .entry-utility -->
-<?php comments_template(); ?>
+			
+						<?php //comments_template(); ?>
 					</div><!-- .entry-content -->
 				</article>
-<?php endwhile; ?>
+			
+			<?php endwhile; ?>
+		</div><!--/columns-->
+				
+	</div><!--/row-->
+</div><!--/container-->
+
 <?php get_footer(); ?>
