7259071741e70f2fcfe9655cd953f03dd43e5a4a
Chris Blakley <greatblakes@gmail.com>
2014-07-11 12:40:09 -0400

    Used 'auto' as second Google Analytics parameter

--- functions.php
+++ functions.php
@@ -407,7 +407,7 @@ function custom_login_css() {
 	    echo '<script type="text/javascript" src="' . get_bloginfo('template_directory') . '/js/login.js"></script>';
 	    
 	    //@TODO: Need to figure out a way to automate the Google Analytics account number and domain!
-	    echo "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-00000000-1', 'domainnamegoeshere.com');</script>";
+	    echo "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-00000000-1', 'auto');</script>";
 	}
 }
 
--- header.php
+++ header.php
@@ -92,7 +92,7 @@
 			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 			  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 			
-			  ga('create', 'UA-00000000-1', 'domainnamegoeshere.com'); <?php //@TODO: Uncomment this script, and update the Google Analytics ID and domain. Don't forget to update the ID in the functions.php file too! ?>
+			  ga('create', 'UA-00000000-1', 'auto'); <?php //@TODO: Uncomment this script, and update the tracking ID. Don't forget to update the ID in the functions.php file too! ?>
 			  ga('send', 'pageview');
 			</script>
 		-->
