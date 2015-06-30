3abdb7c43ea0b78c36ec2217bde983bf6744d7fe
Chris Blakley <greatblakes@gmail.com>
2015-06-30 15:23:56 -0400

    Better no-js tracking

--- includes/no-js.php
+++ includes/no-js.php
@@ -1,5 +1,5 @@
 <?php
-	if ( file_exists('../../../../wp-load.php') ) {
+	if ( file_exists('../../../../wp-load.php') ){
 		require_once('../../../../wp-load.php'); //@TODO "Nebula" 0: If these are being used to include separate sections of a template from independent files, then get_template_part() should be used instead.
 
 		/*
@@ -8,15 +8,19 @@
 			$_GET['t'] is urlencode(get_the_title($post->ID));
 		*/
 
-		//@TODO "Nebula" 0: Should only send these if not bot traffic. Look for an actual browser or OS (either mobile detect or OS detect).
-		if ( 1==1 ){
-			ga_send_pageview($_GET['h'], $_GET['p'], $_GET['t']);
-			ga_send_event('JavaScript Disabled', $_SERVER['HTTP_USER_AGENT'], $_GET['t'], null, 1);
+		$bots = array('bot', 'crawl', 'spider');
+		foreach($bots as $bot){
+			if ( strpos(strtolower($_SERVER['HTTP_USER_AGENT']), $bot) !== false ){
+				die;
+			}
 		}
 
-		//Parse detected User Agents here: http://udger.com/resources/online-parser
+        ga_send_pageview($_GET['h'], $_GET['p'], $_GET['t']);
+		ga_send_event('JavaScript Disabled', $_SERVER['HTTP_USER_AGENT'], $_GET['t'], null, 1);
+
+		//Parse detected User Agents here: http://udger.com/resources/online-parser (or use Google Analytics "Browser" as a secondary dimension).
 
 	} else {
-		die('File does not exist.');
+		die('Required file does not exist.');
 	}
 ?>
