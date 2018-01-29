bfbfe347c1d8712f91009e48e08b2c076a00baa0
Chris Blakley <greatblakes@gmail.com>
2018-01-28 22:06:53 -0500

    Nebula object hotfix
    This is a fix in case Google Analytics is not enabled.

--- Nebula-Child/resources/sw.js
+++ Nebula-Child/resources/sw.js
@@ -3,7 +3,7 @@
 //@TODO: Enable "Service Worker" in Nebula Options (under Functions)
 
 //BEGIN Automated edits. These will be automatically overwritten.
-var CACHE_NAME = 'nebula-nebula-child-43829'; //Sunday, January 28, 2018 9:23:50 PM
+var CACHE_NAME = 'nebula-nebula-child-31841'; //Sunday, January 28, 2018 10:04:48 PM
 var OFFLINE_URL = 'https://gearside.com/nebula/offline/';
 var OFFLINE_IMG = 'https://gearside.com/nebula/wp-content/themes/Nebula-master/assets/img/offline.svg';
 var META_ICON = 'https://gearside.com/nebula/wp-content/themes/Nebula-master/assets/img/meta/android-chrome-512x512.png';
