5b5428e13097e6e1165310cb4c8e5894d8e737a5
Chris Blakley <greatblakes@gmail.com>
2019-05-11 13:53:05 -0400

    Version 7 Release

--- Nebula-Child/resources/sw.js
+++ Nebula-Child/resources/sw.js
@@ -1,6 +1,6 @@
 //BEGIN automated edits. These will be automatically overwritten.
 var THEME_NAME = 'nebula-child';
-var NEBULA_VERSION = 'v6.12.9.9104'; //Friday, May 10, 2019 11:58:33 PM
+var NEBULA_VERSION = 'v7.0.11.5757'; //Saturday, May 11, 2019 1:51:20 PM
 var OFFLINE_URL = 'https://gearside.com/nebula/offline/';
 var OFFLINE_IMG = 'https://gearside.com/nebula/wp-content/themes/Nebula-master/assets/img/offline.svg';
 var OFFLINE_GA_DIMENSION = 'cd2';
