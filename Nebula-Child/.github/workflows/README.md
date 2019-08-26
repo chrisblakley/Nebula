7f9bd02dd54db6f92b221be05b019a8e5819e7b2
Chris Blakley <greatblakes@gmail.com>
2019-08-25 22:11:30 -0400

    FA update, other minor edits, WPT action tweaks
    #1946 #1957

---
+++ Nebula-Child/.github/workflows/README.md
@@ -0,0 +1,1 @@
+These Github Actions workflows are bundled with Nebula as they may be useful when committing to Github. To use these, simply move this workflow directory to the root /.github/ directory. Additional customization may be needed by creating Secrets for use by these Actions.
--- Nebula-Child/resources/sw.js
+++ Nebula-Child/resources/sw.js
@@ -1,6 +1,6 @@
 //BEGIN automated edits. These will be automatically overwritten.
 var THEME_NAME = 'nebula-child';
-var NEBULA_VERSION = 'v7.3.23.9811'; //Friday, August 23, 2019 12:14:07 AM
+var NEBULA_VERSION = 'v7.3.25.9160'; //Sunday, August 25, 2019 9:59:06 PM
 var OFFLINE_URL = 'https://gearside.com/nebula/offline/';
 var OFFLINE_IMG = 'https://gearside.com/nebula/wp-content/themes/Nebula-master/assets/img/offline.svg';
 var OFFLINE_GA_DIMENSION = 'cd2';
