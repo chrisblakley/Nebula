da58f14b8a1499e865c716770d649c2c096ef4d1
Chris Blakley <greatblakes@gmail.com>
2020-05-12 00:00:50 -0400

    Codacy code quality improvements
    Part 1 of ???

--- assets/js/shortcodes.js
+++ assets/js/shortcodes.js
@@ -12,7 +12,8 @@
 					ed.focus();
 					ed.selection.setContent('[accordion type="single"]<br />[accordion_item title="TITLE_HERE" default="open"]CONTENT_HERE[/accordion_item]<br />[/accordion]');
 				}
-			}),
+			});
+
 			ed.addButton('nebulabutton', {
 				title: 'Insert Button',
 				image: nebula.site.directory.template.uri + '/assets/img/admin/nebulabutton.png',
@@ -21,7 +22,8 @@
 					ed.focus();
 					ed.selection.setContent('[button href=http://pinckneyhugo.com/ target=_blank]Click Here[/button]');
 				}
-			}),
+			});
+
 			ed.addButton('nebulaclear', {
 				title: 'Insert Clear',
 				image: nebula.site.directory.template.uri + '/assets/img/admin/nebulaclear.png',
@@ -30,7 +32,8 @@
 					ed.focus();
 					ed.selection.setContent('[clear]');
 				}
-			}),
+			});
+
 			ed.addButton('nebulacode', {
 				title: 'Insert Code',
 				type: 'menubutton',
@@ -57,7 +60,8 @@
 						ed.selection.setContent('[gist file=FILENAME lang=LANGUAGE]URL[/gist]');
 					}
 				}]
-			}),
+			});
+
 			ed.addButton('nebuladiv', {
 				title: 'Insert Div',
 				image: nebula.site.directory.template.uri + '/assets/img/admin/nebuladiv.png',
@@ -68,7 +72,8 @@
 					var shortcodeContent = ( tinyMCE.activeEditor.selection.getContent() !== '' )? tinyMCE.activeEditor.selection.getContent() : 'CONTENT_HERE';
 					ed.selection.setContent('[div class="CLASSES" style=STYLES]' + shortcodeContent + '[/div]');
 				}
-			}),
+			});
+
 			ed.addButton('nebulacolgrid', {
 				title: 'Insert Grid',
 				type: 'menubutton',
@@ -96,7 +101,8 @@
 						ed.selection.setContent('[columns scale=md columns=12 class="CLASSES" style="STYLES"]' + shortcodeContent + '[/columns]');
 					}
 				}]
-			}),
+			});
+
 			ed.addButton('nebulaicon', {
 				title: 'Insert Icon',
 				type: 'splitbutton',
@@ -112,7 +118,8 @@
 						window.open('https://fontawesome.com/icons?d=gallery', '_blank');
 					}
 				}]
-			}),
+			});
+
 			ed.addButton('nebulaline', {
 				title: 'Insert Line',
 				image: nebula.site.directory.template.uri + '/assets/img/admin/nebulaline.png',
@@ -121,7 +128,8 @@
 					ed.focus();
 					ed.selection.setContent('[line space=5]');
 				}
-			}),
+			});
+
 			ed.addButton('nebulamap', {
 				title: 'Insert Google Map',
 				type: 'splitbutton',
@@ -156,7 +164,8 @@
 						ed.selection.setContent('[map mode=view center="43.0536364,-76.1657063" zoom=19 maptype=satellite]');
 					}
 				}]
-			}),
+			});
+
 			ed.addButton('nebulaslider', {
 				title: 'Insert Slider',
 				image: nebula.site.directory.template.uri + '/assets/img/admin/nebulaslider.png',
@@ -165,7 +174,8 @@
 					ed.focus();
 					ed.selection.setContent('[slider frame status]<br />[slide title="TITLE_HERE" link=http://www.pinckneyhugo.com target=_blank]IMAGE_URL_HERE[/slide]<br />[/slider]');
 				}
-			}),
+			});
+
 			ed.addButton('nebulaspace', {
 				title: 'Insert Vertical Space',
 				image: nebula.site.directory.template.uri + '/assets/img/admin/nebulaspace.png',
@@ -174,7 +184,8 @@
 					ed.focus();
 					ed.selection.setContent('[space height=25]');
 				}
-			}),
+			});
+
 			ed.addButton('nebulatooltip', {
 				title: 'Insert Tooltip',
 				image: nebula.site.directory.template.uri + '/assets/img/admin/nebulatooltip.png',
@@ -184,7 +195,8 @@
 					var shortcodeContent = ( tinyMCE.activeEditor.selection.getContent() !== '' )? tinyMCE.activeEditor.selection.getContent() : 'CONTENT_HERE';
 					ed.selection.setContent('[tooltip tip="BUBBLE_TEXT_HERE"]' + shortcodeContent + '[/tooltip]');
 				}
-			}),
+			});
+
 			ed.addButton('nebulavideo', {
 				title: 'Insert Video',
 				type: 'splitbutton',
