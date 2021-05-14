1ddeced43f189a728d0772243205be829fc1c699
Chris Blakley <chrisb@pinckneyhugo.com>
2021-05-14 10:02:40 -0400

    Bootstrap update

--- Nebula-Child/resources/sw.js
+++ Nebula-Child/resources/sw.js
@@ -1,6 +1,6 @@
 //BEGIN automated edits. These will be automatically overwritten.
 const THEME_NAME = 'nebula-child';
-const NEBULA_VERSION = 'v9.0.12.736'; //Wednesday, May 12, 2021 5:40:05 PM
+const NEBULA_VERSION = 'v9.0.14.416'; //Friday, May 14, 2021 9:59:12 AM
 const OFFLINE_URL = 'https://nebula.gearside.com/offline/';
 const OFFLINE_IMG = 'https://nebula.gearside.com/wp-content/themes/Nebula-main/assets/img/offline.svg';
 const OFFLINE_GA_DIMENSION = 'cd2';
