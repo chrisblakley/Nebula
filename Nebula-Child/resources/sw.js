188bd955ca3880680294ef739d149fe51e2dd62d
Chris Blakley <chrisb@pinckneyhugo.com>
2021-06-28 10:17:13 -0400

    Added start of XD design file

--- Nebula-Child/resources/sw.js
+++ Nebula-Child/resources/sw.js
@@ -1,6 +1,6 @@
 //BEGIN automated edits. These will be automatically overwritten.
 const THEME_NAME = 'nebula-child';
-const NEBULA_VERSION = 'v9.1.23.692'; //Wednesday, June 23, 2021 4:36:42 PM
+const NEBULA_VERSION = 'v9.1.28.426'; //Monday, June 28, 2021 10:13:18 AM
 const OFFLINE_URL = 'https://nebula.gearside.com/offline/';
 const OFFLINE_IMG = 'https://nebula.gearside.com/wp-content/themes/Nebula-main/assets/img/offline.svg';
 const OFFLINE_GA_DIMENSION = 'cd2';
