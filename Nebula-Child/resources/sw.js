f9a4f13a98f63141e4964bd3e42b2fad73082819
Chris Blakley <chrisb@pinckneyhugo.com>
2021-06-29 16:13:14 -0400

    Added key Bootstrap 5 components to the XD design resource file
    #2165

--- Nebula-Child/resources/sw.js
+++ Nebula-Child/resources/sw.js
@@ -1,6 +1,6 @@
 //BEGIN automated edits. These will be automatically overwritten.
 const THEME_NAME = 'nebula-child';
-const NEBULA_VERSION = 'v9.1.28.426'; //Monday, June 28, 2021 10:13:18 AM
+const NEBULA_VERSION = 'v9.1.29.674'; //Tuesday, June 29, 2021 4:10:17 PM
 const OFFLINE_URL = 'https://nebula.gearside.com/offline/';
 const OFFLINE_IMG = 'https://nebula.gearside.com/wp-content/themes/Nebula-main/assets/img/offline.svg';
 const OFFLINE_GA_DIMENSION = 'cd2';
